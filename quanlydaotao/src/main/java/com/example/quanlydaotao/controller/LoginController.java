2e2f03abbdb47e38c8fa01a3218aadb9b4f2ce9e
Quin19FD <nguyenhoangquyen2004bp@gmail.com>
2025-04-04 21:54:21 +0700

    Initial commit

---
+++ quanlydaotao/src/main/java/com/example/quanlydaotao/controller/LoginController.java
@@ -0,0 +1,21 @@
+//package com.example.quanlydaotao.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class LoginController {
+//
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login"; // Trả về tên file login.html
+//    }
+//
+//    // Thêm endpoint test
+//    @GetMapping("/test")
+//    @ResponseBody
+//    public String test() {
+//        return "Test successful!";
+//    }
+//}
