a81135f96f0b93c4d04fec64f6b5ae34812513be
Mai Anh Kiet <anhkiet91112004@gmail.com>
2025-05-22 10:08:42 +0700

    done

--- quanlydaotao/src/main/java/com/example/quanlydaotao/repository/KeHoachDayHocRepo.java
+++ quanlydaotao/src/main/java/com/example/quanlydaotao/repository/KeHoachDayHocRepo.java
@@ -1,7 +1,32 @@
 package com.example.quanlydaotao.repository;
 
 import com.example.quanlydaotao.model.KeHoachDayHoc;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface KeHoachDayHocRepo extends JpaRepository<KeHoachDayHoc, Integer> {
+
+    @Query("SELECT MAX(k.maKeHoach) FROM KeHoachDayHoc k")
+    Integer findMaxMaKeHoach();
+
+
+    @EntityGraph(attributePaths = {"maHocPhan"})
+    @Override
+    List<KeHoachDayHoc> findAll();
+
+    @EntityGraph(attributePaths = {"maHocPhan"})
+    @Override
+    Optional<KeHoachDayHoc> findById(Integer maKeHoach);
+
+    // Tìm kiếm theo mã học phần
+    @EntityGraph(attributePaths = {"maHocPhan"})
+    List<KeHoachDayHoc> findByMaHocPhan_MaHocPhan(String maHocPhan);
+
+
 }
